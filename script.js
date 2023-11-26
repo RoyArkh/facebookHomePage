201811c739b966e34547d5b09227258bd220eb21
Roya Arkhmammadova <151827017+RoyArkh@users.noreply.github.com>
2023-11-26 18:50:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function func() {
+    document.getElementById("head1").innerHTML = "twitter";
+    document.getElementById("head1").style.color = "#1DA1F2"
+    document.getElementById("head2").innerHTML = "Elon Musk got everything <br> you need!";
+}
